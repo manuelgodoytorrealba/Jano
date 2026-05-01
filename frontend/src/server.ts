14cc00b5c8acdcb267131a011b762206d60b0f93
manuelgodoytorrealba <manuelgodoytorrealba@icloud.com>
2026-05-01 13:41:46 +0200

    allow fix for deploy

--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -115,13 +115,14 @@ app.use((req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
+  const port = Number(process.env['PORT']) || 4200;
+
+  app.listen(port, '0.0.0.0', (error) => {
     if (error) {
       throw error;
     }
 
-    console.log(`Node Express server listening on http://localhost:${port}`);
+    console.log(`🚀 SSR running on http://localhost:${port}`);
   });
 }
 
